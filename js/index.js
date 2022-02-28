75d90f536a66c620df6cdc3ed9149f54dfe0300b
Jofran8 <molotov.gg42@gmail.com>
2022-02-28 01:17:21 -0500

    login prompt

---
+++ js/index.js
@@ -0,0 +1,22 @@
+'use strict';
+const documentReady = () => {
+    
+    const phrases = ["😘 Welcome ", "👽 Have a good time ", "😎 You're the best, welcome "];
+
+    const random = Math.floor(Math.random() * phrases.length);
+    
+    let name = prompt("Hi, welcome to the Book Store, enter your name: ")
+    try { 
+        while(!/^[a-zA-Z]+$/.test(name)) { 
+            name= prompt("Please re-enter your name, letters only.");
+        }
+        alert(phrases[random]);
+    }catch(e){
+    alert(e) 
+    }
+
+
+
+
+};
+document.addEventListener('DOMContentLoaded', documentReady);
