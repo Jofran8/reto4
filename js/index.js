7c67ab7acf12c47a889a6f51db44089cc5e2e193
Jofran8 <molotov.gg42@gmail.com>
2022-02-28 02:04:52 -0500

    message-js

--- js/index.js
+++ js/index.js
@@ -1,18 +1,44 @@
-'use strict';
+"use strict";
 const documentReady = () => {
-    
-    const phrases = ["😘 Welcome ", "👽 Have a good time ", "😎 You're the best, welcome "];
+  const phrases = [
+    "😘 Welcome ",
+    "👽 Have a good time ",
+    "😎 You're the best, welcome ",
+  ];
 
-    const random = Math.floor(Math.random() * phrases.length);
-    
-    let name = prompt("Hi, welcome to the Book Store, enter your name: ")
-    try { 
-        while(!/^[a-zA-Z]+$/.test(name)) { 
-            name= prompt("Please re-enter your name, letters only.");
-        }
-        alert(phrases[random]);
-    }catch(e){
-    alert(e) 
+  const random = Math.floor(Math.random() * phrases.length);
+
+  let name = prompt("Hi, welcome to the Book Store, enter your name: ");
+  try {
+    while (!/^[a-zA-Z]+$/.test(name)) {
+      name = prompt("Please re-enter your name, letters only.");
     }
-}
-document.addEventListener('DOMContentLoaded', documentReady);
+    alert(phrases[random]);
+  } catch (e) {
+    alert(e);
+  }
+
+  const button = document.querySelector("button");
+  const message = document.querySelector(".message-container");
+
+  button.addEventListener("click", () => {
+    message.style.display = "block";
+  });
+
+  message.addEventListener("click", (event) => {
+    const classNameOfClickedElement = event.target.classList[0];
+
+    const shouldCloseMessage = classNameOfClickedElement === "message-link";
+    if (shouldCloseMessage) {
+      console.log(classNameOfClickedElement);
+      message.style.display = "none";
+      setTimeout(function () {
+        window.open(
+          "https://www.amazon.com/-/es/amazon-books/b?ie=UTF8&node=13270229011",
+          "_blank"
+        );
+      }, 500);
+    } else message.style.display = "none";
+  });
+};
+document.addEventListener("DOMContentLoaded", documentReady);
